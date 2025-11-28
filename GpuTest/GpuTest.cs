5ced6d490ef6a88d814520f6a66f7938fe746def
VartanovOleksii <o.o.vartanov@student.khai.edu>
2025-11-28 17:39:39 +0100

    Add MSTest solution

--- GpuTest/GpuTest.cs
+++ GpuTest/GpuTest.cs
@@ -3,9 +3,33 @@
     [TestClass]
     public sealed class GpuTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Gpu gpu;
+
+        [TestInitialize]
+        public void Setup()
         {
+            gpu = new Gpu();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            gpu = null;
+        }
+        
+        [TestMethod]
+        public void YearsSinceRelease_date_before_release()
+        {
+            //Arrange
+            gpu.ReleaseDate = DateTime.Parse("18.05.2020");
+            DateTime selected_date = DateTime.Parse("03.05.1976");
+            int expected = -1;
+
+            //Act
+            int actual = gpu.YearsSinceRelease(selected_date);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
